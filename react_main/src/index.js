16abbfbd088a81bc9000de0bb2273b70f3124251
Dheeraj Raikwar <dheeraj@emoment.in>
2021-04-18 11:02:10 +0530

    Say Hello

--- react_main/src/index.js
+++ react_main/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import reportWebVitals from './reportWebVitals';
+
 ReactDOM.render(
  
   <h1> Hello Dheeraj! Happy Learning </h1>,
