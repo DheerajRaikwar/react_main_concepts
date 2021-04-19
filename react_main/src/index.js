1397de10cb06a0f9f5fcc430512b262e55dbd09b
Dheeraj Raikwar <dheeraj@emoment.in>
2021-04-19 18:13:32 +0530

    Concepts

--- react_main/src/index.js
+++ react_main/src/index.js
@@ -4,11 +4,67 @@ import './index.css';
 
 import reportWebVitals from './reportWebVitals';
 
-const Jsx = 'Use JSX';
-const ele = <h2> Using jsx element here = {Jsx} </h2>;
+/*
+--------Here using Jsx --------- */
+function formatName(usr){
+  return usr.firstName + ' ' +usr.lastName;
+}
+const usr = {
+  firstName: 'Dheeraj',
+  lastName: 'raikwar'
+};
+
+const element = <h2> Hello, {formatName(usr)}</h2>;
+
+
+const reactUser = (
+  <h1 className="Hello">
+    Hello react user
+  </h1>
+);
+
+function formatDate(date)
+{
+  return date.toLocaleDateString();
+}
+
+const comment = {
+  date: new Date(),
+  text: 'We are learning react!',
+  author:
+  {
+    name: 'Dheeraj',
+    age: '21'
+  },
+}
+
+
+function Comment(props){
+  return(
+    <div className="Comment">
+      <div className="UserInfo">
+        <h1> Hello, i am {props.author.name}</h1>
+        <div className="UserAge">
+          <p> Age of dheeraj:  {props.author.age} </p>
+         
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+
+      </div>
+    </div>
+  );
+}
 
 ReactDOM.render(
-  ele,
+  <Comment
+  date={comment.date}
+  text={comment.text}
+  author={comment.author} />,
   document.getElementById('root')
 );
 
